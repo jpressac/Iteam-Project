b295f47be11a2231d802bee3d6612917549b913a
valerandanne <valentinarandanne@gmail.com>
2016-05-21 09:44:09 -0300

    starting registration form

---
+++ Iteam-project/src/components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,46 @@
+import React, {Component,PropTypes} from 'react';
+import classes from ''
+class RegistrationForm extends Component {
+
+  render(){
+    return(
+    <form role="form" className=classes.form>
+    <label for="nombre completo"> Nombre completo </label>
+                  <div class="row" id="nombre completo">
+                      <div class="col-md-6">
+                          <div class="form-group input-group">
+                              <span class="input-group-btn"></span>
+                              <input class="form-control" type="text" placeholder="Nombre" id="nombre"></input>
+                          </div>
+                      </div>
+                      <div class="col-md-6">
+                          <div class="form-group input-group">
+                              <span class="input-group-btn"></span>
+                              <input class="form-control" type="text" placeholder="Apellido" id="apellido"></input>
+                          </div>
+                      </div>
+                      <div class="col-md-8">
+                          <div class="form-group">
+                              <label class="control-label">Nacionalidad</label>
+                              <div class="selectContainer">
+                                  <select class="form-control" name="nacionalidad">
+                                      <option value=""></option>
+                                      <option value="arg">Argentina</option>
+                                      <option value="br">Brasil</option>
+                                      <option value="usa">EEUU</option>
+                                  </select>
+                              </div>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                              <label class="control-label">Profesión</label>
+                                <span class="input-group-btn"></span>
+                                <input class="form-control" type="text" placeholder="Profesión" id="profesión"></input>
+                            </div>
+                        </div>
+                  </div>
+              </form>);
+  };
+}
+export default RegistrationForm;
--- Iteam-project/src/routes/Home/components/HomeView.js
+++ Iteam-project/src/routes/Home/components/HomeView.js
@@ -2,14 +2,16 @@
 import React from 'react'
 import LightBulbImage from '../assets/LightBulb.jpg'
 import classes from './HomeView.scss'
+import RegistrationForm from '../../../components/RegistrationForm'
 
 export const HomeView = () => (
   <div className={classes.homeDiv}>
-    
+
     <img align="center"
       alt='This is a duck, because Redux!'
       className={classes.image}
       src={LightBulbImage} />
+      <RegistrationForm/>
   </div>
 )
 
