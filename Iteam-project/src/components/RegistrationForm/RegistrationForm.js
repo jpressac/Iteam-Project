23ac645503cea453e3ac02737ea6bd38c5fb8c7f
valerandanne <valentinarandanne@gmail.com>
2016-06-10 20:26:11 -0300

    register with axios

--- Iteam-project/src/components/RegistrationForm/RegistrationForm.js
+++ Iteam-project/src/components/RegistrationForm/RegistrationForm.js
@@ -3,62 +3,94 @@ import classes from './RegistrationForm.scss'
 import TextBox from '../Form/TextBox/TextBox.js'
 import RadioButton from '../Form/RadioButton/RadioButton.js'
 import { Button } from 'react-bootstrap';
-//import DatePicker from '../Form/DatePicker/DatePicker.js'
+import {connect} from 'react-redux';
+import {submitUser} from '../../redux/RegistrationForm/actions.js'
 
 class RegistrationForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      nationality: '',
+      mail: '',
+      male: '',
+      female: '',
+      profession: '',
+      username: '',
+      password: '',
+      repeatPassword: ''
+    }
+  }
+  handleClick(){
+      console.dir(this.refs);
+      console.log(this.refs.firstName.value)
 
-  render(){
-    return(
-    <form role="form" className={classes.form}>
-    <label for="nombre completo"> Nombre completo </label>
+      this.setState({
+        firstName: this.refs.firstName.value,
+        lastName: this.refs.lastName.value,
+      //nationality: this.refs.nationality.value,
+         mail: this.refs.mail.value,
+         male: this.refs.male.value,
+         female: this.refs.female.value,
+         profession: this.refs.profession.value,
+         username: this.refs.username.value,
+         password: this.refs.password.value,
+         repeatPassword: this.refs.repeatPassword.value
+      });
+
+      console.log(this.state);
+      submitUser(this.state);
+    }
+    render(){
+      return(
+        <form role="form" ref="form">
+        <label for="nombre completo"> Nombre completo </label>
                   <div class="row" id="datos_personales">
-                      <div class="col-md-6">
-                          <TextBox label="First name"></TextBox>
+                      <div className="form-inline" class="col-md-6">
+                      <label> Nombre </label>
+                          <input type="text" className="form-control" label="First name" ref="firstName" ></input>
                       </div>
-                      <div class="col-md-6">
-                          <TextBox label="Last name"></TextBox>
+                      <div className="form-inline" class="col-md-6">
+                          <label> Apellido </label>
+                          <input label="Last name" className="form-control" ref="lastName"></input>
                       </div>
-                      <div class="col-md-8">
+                      <div className="form-inline" class="col-md-8">
                           <div class="form-group">
-                              <label class="control-label">Nacionalidad</label>
-                              <div class="selectContainer">
-                                  <select class="form-control" name="nacionalidad">
-                                      <option value=""></option>
-                                      <option value="arg">Argentina</option>
-                                      <option value="br">Brasil</option>
-                                      <option value="usa">EEUU</option>
-                                  </select>
-                              </div>
+                              <label className="control-label" >Nacionalidad</label>
                           </div>
+                      </div>
+                      <div className="form-inline" class="col-md-6">
+                            <label className="control-label" >Mail</label>
+                            <input type="email" label="Mail" className="form-control" ref="mail"></input>
                         </div>
                         <div class="col-md-6">
-                          //TODO datepicker
+                        <label className="control-label" >Male</label>
+                            <input type="radio" className="form-control" groupName="Sexo" ref="male"></input>
+                        <label className="control-label" >Male</label>    
+                            <input type="radio" className="form-control" groupName="Sexo" ref="female"></input>
+                        </div>
+                        <div className="form-inline" class="col-md-6">
+                              <label className="control-label" >Profesión</label>
+                            <input type="text" className="form-control" ref="profession"></input>
+                        </div>
+                        <div className="form-inline" class="col-md-6">
+                              <label className="control-label" >Nombre de usuario</label>
+                            <input type="text" className="form-control" ref="username"></input>
+                        </div>
+                        <div className="form-inline" class="col-md-6">
+                              <label className="control-label" >Password</label>
+                            <input type="password" label="Password" className="form-control" ref="password"></input>
+                        </div>
+                        <div className="form-inline" class="col-md-6">
+                              <label className="control-label" >Repeat Password</label>
+                            <input type="password" label="Repeat password" className="form-control" ref="repeatPassword"></input>
                         </div>
                         <div class="col-md-6">
-                            <TextBox label="Mail"></TextBox>
-                        </div>
-                        <div class="col-md-6">
-                            <RadioButton groupName="Sexo" label="Male"></RadioButton>
-                            <RadioButton groupName="Sexo" label="Female"></RadioButton>
-                        </div>
-                        <div class="col-md-6">
-                            <TextBox label="Profession"></TextBox>
-                        </div>
-                        <div class="col-md-6">
-                            <TextBox label="User Name"></TextBox>
-                        </div>
-                        <div class="col-md-6">
-                            <TextBox label="Password"></TextBox>
-                        </div>
-                        <div class="col-md-6">
-                            <TextBox label="Repeat password"></TextBox>
-                        </div>
-                        <div class="col-md-6">
-                            <Button type="button" bsStyle="success">Submit</Button>
+                            <Button  bsStyle="success" onClick={this.handleClick.bind(this)} >Register</Button>
                         </div>
                       </div>
               </form>);
   };
 }
-//DatePicker selected={this.state.date} onChange={this.handleChange}></DatePicker>
-export default RegistrationForm;
+export default RegistrationForm
--- Iteam-project/src/store/reducers.js
+++ Iteam-project/src/store/reducers.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
+import {registerReducer} from '../redux/RegistrationForm/reducer.js'
+import Immutable from 'immutable';
+import {reducer as formReducer} from 'redux-form';
 
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
     // Add sync reducers here
     router,
+    
     ...asyncReducers
   })
 }
