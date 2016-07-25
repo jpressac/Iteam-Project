d7f6d73ee7587c5ff33dc5aa943c62069d28d602
jpressac <pressaccojuan@gmail.com>
2016-07-25 11:38:29 -0300

    Update login page, and change authentication controller

--- public-api/src/main/java/org/iteam/data/model/User.java
+++ public-api/src/main/java/org/iteam/data/model/User.java
@@ -9,135 +9,136 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
 
-    // TODO: add creation/modification date
-    private String username;
-    private String password;
-    private String name;
-    private String lastName;
-    private String nationality;
-    private String bornDate;
-    private List<String> hobbies;
-    private String profession;
-    private String mbtiTest;
-    private String discTest;
-    private boolean logicalDelete = false;
-    private String insertionDate;
-    private String mail;
+	// TODO: add creation/modification date
+	private String username;
+	// TODO: password must not be returned to the UI
+	private String password;
+	private String name;
+	private String lastName;
+	private String nationality;
+	private String bornDate;
+	private List<String> hobbies;
+	private String profession;
+	private String mbtiTest;
+	private String discTest;
+	private boolean logicalDelete = false;
+	private String insertionDate;
+	private String mail;
 
-    public User(boolean logicalDelete) {
-        this.logicalDelete = logicalDelete;
-    }
+	public User(boolean logicalDelete) {
+		this.logicalDelete = logicalDelete;
+	}
 
-    public User() {
+	public User() {
 
-    }
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String userName) {
-        this.username = userName;
-    }
+	public void setUsername(String userName) {
+		this.username = userName;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public String getNationality() {
-        return nationality;
-    }
+	public String getNationality() {
+		return nationality;
+	}
 
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
 
-    public String getBornDate() {
-        return bornDate;
-    }
+	public String getBornDate() {
+		return bornDate;
+	}
 
-    public void setBornDate(String bornDate) {
-        this.bornDate = bornDate;
-    }
+	public void setBornDate(String bornDate) {
+		this.bornDate = bornDate;
+	}
 
-    public List<String> getHobbies() {
-        return hobbies;
-    }
+	public List<String> getHobbies() {
+		return hobbies;
+	}
 
-    public void setHobbies(List<String> hobbies) {
-        this.hobbies = hobbies;
-    }
+	public void setHobbies(List<String> hobbies) {
+		this.hobbies = hobbies;
+	}
 
-    public String getProfession() {
-        return profession;
-    }
+	public String getProfession() {
+		return profession;
+	}
 
-    public void setProfession(String profession) {
-        this.profession = profession;
-    }
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
 
-    public String getMbtiTest() {
-        return mbtiTest;
-    }
+	public String getMbtiTest() {
+		return mbtiTest;
+	}
 
-    public void setMbtiTest(String mbtiTest) {
-        this.mbtiTest = mbtiTest;
-    }
+	public void setMbtiTest(String mbtiTest) {
+		this.mbtiTest = mbtiTest;
+	}
 
-    public String getDiscTest() {
-        return discTest;
-    }
+	public String getDiscTest() {
+		return discTest;
+	}
 
-    public void setDiscTest(String discTest) {
-        this.discTest = discTest;
-    }
+	public void setDiscTest(String discTest) {
+		this.discTest = discTest;
+	}
 
-    public boolean isLogicalDelete() {
-        return logicalDelete;
-    }
+	public boolean isLogicalDelete() {
+		return logicalDelete;
+	}
 
-    public void setLogicalDelete(boolean logicalDelete) {
-        this.logicalDelete = logicalDelete;
-    }
+	public void setLogicalDelete(boolean logicalDelete) {
+		this.logicalDelete = logicalDelete;
+	}
 
-    public String getMail() {
-        return mail;
-    }
+	public String getMail() {
+		return mail;
+	}
 
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
 
-    public String getInsertionDate() {
-        return insertionDate;
-    }
+	public String getInsertionDate() {
+		return insertionDate;
+	}
 
-    public void setInsertionDate(String insertionDate) {
-        this.insertionDate = insertionDate;
-    }
+	public void setInsertionDate(String insertionDate) {
+		this.insertionDate = insertionDate;
+	}
 
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
-    }
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 }
