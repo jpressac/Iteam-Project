f90051186f7252293a9ef5b981a91c99ef49e582
valerandanne <valentinarandanne@gmail.com>
2016-08-09 21:08:38 -0300

    Reducers

--- Iteam-project/src/redux/rootReducer.js
+++ Iteam-project/src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
+import boardReducer from './reducers/BoardReducer';
 
 export default combineReducers({
-  router
+  router,
+  boardReducer
 })
