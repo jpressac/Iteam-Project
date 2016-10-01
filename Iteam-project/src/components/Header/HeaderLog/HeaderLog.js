90cdffcbade4a3a9739502f2e5219750e839105b
agusdemalde <agus.demalde@gmail.com>
2016-10-01 18:13:50 -0300

    Header log and team view

--- Iteam-project/src/components/Header/HeaderLog/HeaderLog.js
+++ Iteam-project/src/components/Header/HeaderLog/HeaderLog.js
@@ -1,14 +1,13 @@
 import React, {Component, PropTypes} from 'react';
-import {IndexLink, Link} from 'react-router'
+import { Link} from 'react-router'
 import classes from './HeaderLog.scss'
 import logo from '../image/logo.png'
 import {PATHS} from '../../../constants/routes'
 import {connect} from 'react-redux'
 import AppBar from 'react-toolbox/lib/app_bar'
 import Navigation from 'react-toolbox/lib/navigation'
-import {logout} from '../../../redux/reducers/Login/LoginUser'
 import LogoutButton from './LogoutButton'
-import {push} from 'react-router-redux'
+import {Button, IconButton} from 'react-toolbox/lib/button';
 
 const mapStateToProps = (state)=> {
   if (state.loginUser !== null) {
@@ -19,47 +18,41 @@ const mapStateToProps = (state)=> {
 };
 
 
-
-
 class HeaderLog extends Component {
 
   constructor(props) {
     super(props);
   }
 
-    teamClick=() =>  {
-      push('/' + PATHS.MENULOGGEDIN.NEWTEAM)
-    };
-
-   actions = [
-    { label: 'Team', raised: true, icon: 'access_alarm', onClick:this.teamClick()},
-    { label: 'Location', raised: true, accent: true, icon: 'room'}
-  ];
+    // teamClick=() =>  {
+    //   dispatch(push('/' + PATHS.MENULOGGEDIN.NEWTEAM))
+    // };
+  //
+  //  actions = [
+  //   { label: 'Team', raised: true, icon: 'access_alarm', onClick:this.teamClick},
+  //   { label: 'Location', raised: true, accent: true, icon: 'room'}
+  // ];
 
   render() {
     return (
 
     <header >
 
-      <AppBar fixed flat className={classes.appBar} >
-        <a href="/home">LOGO</a>
-        <div>
-          <Navigation type='horizontal'  actions={actions}  />
-            <Navigation type='horizontal'  >
-            <Link id="home" className={classes.menus}  to={'/' + PATHS.MENULOGGEDIN.HOME} activeClassName="active">HOME</Link>
+      <AppBar fixed flat  >
+        <a href="/home"></a>
+        <div className={classes.div}>
+          <Navigation type='horizontal' accent >
+             <Link id="home"   className={classes.menus}  to={'/' + PATHS.MENULOGGEDIN.HOME} activeClassName="active">HOME</Link>
             <Link className={classes.menus} to={'/' + PATHS.MENULOGGEDIN.MYMEETINGS} activeClassName="active"> My meetings</Link>
+            <Link className={classes.menus} to={'/' + PATHS.MENULOGGEDIN.MEETING} activeClassName="active"> NEW MEETING</Link>
             <Link className={classes.menus} to={'/' + PATHS.MENULOGGEDIN.PROFILE} activeClassName="active"> MY PROFILE</Link>
             <Link className={classes.menus} to={'/' + PATHS.MENULOGGEDIN.NEWTEAM} activeClassName="active">NEW TEAM </Link>
             <Link className={classes.menus} to={'/' + PATHS.MENULOGGEDIN.BOARD} activeClassName="active"> PERSONAL BOARD </Link>
-            <span className = "glyphicon glyphicon-paperclip" > {this.props.user}</span >  <LogoutButton >Logout</LogoutButton>
-
-
-
+            <span className = "glyphicon glyphicon-user" ><label> {this.props.user}</label></span >
           </Navigation>
         </div>
+        <LogoutButton ></LogoutButton>
       </AppBar>
-
-
 
   </header>
   );
