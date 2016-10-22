d0a3e046e34ef65f192bfe189b79f26d1f47c00b
agusdemalde <agus.demalde@gmail.com>
2016-10-22 16:04:24 -0300

    dispatch header not log

--- Iteam-project/src/components/Header/HeaderNotLog/HeaderNotLog.js
+++ Iteam-project/src/components/Header/HeaderNotLog/HeaderNotLog.js
@@ -1,18 +1,27 @@
 import React, {Component,PropTypes} from 'react';
-import { IndexLink, Link } from 'react-router'
+import {  Link } from 'react-router'
 import { AppBar } from 'react-toolbox/lib/app_bar'
 import themeAppBar from './HeaderNotLog.scss'
 import themeMenu from './menu.scss'
 import themeNav from './nav.scss'
-
-import { Button } from 'react-bootstrap';
+import {Button} from 'react-toolbox/lib/button';
 import logo from '../image/iteamLogo.jpg'
 import { PATHS } from '../../../constants/routes'
-
 import Navigation from 'react-toolbox/lib/navigation'
+import {connect} from 'react-redux'
+import {push} from 'react-router-redux'
+
+const mapDispatchToProps = dispatch => ({
+  home: () => dispatch(push('/' + PATHS.MENUNOTLOGGEDIN.HOME)),
+  about:()=> dispatch(push('/' + PATHS.MENUNOTLOGGEDIN.ABOUT)),
+  contact:()=> dispatch(push('/' + PATHS.MENUNOTLOGGEDIN.CONTACT)),
+  login:()=> dispatch(push('/' + PATHS.MENUNOTLOGGEDIN.LOGIN))
+
+});
 
 class HeaderNotLog extends React.Component {
   render(){
+
     return(
 
       <header >
@@ -22,11 +31,11 @@ class HeaderNotLog extends React.Component {
           <div >
             <img src={logo} style={{height:50,width:100,marginRight:400}}></img>
             <Navigation type="horizontal" theme={themeNav} >
-              <Link id="home"  style={{color:'rgb(136,14,79)', margin:30}} to={'/' + PATHS.MENUNOTLOGGEDIN.HOME} activeClassName="active">HOME</Link>
-                <Link theme={themeMenu.menu} style={{color:'rgb(136,14,79)', margin:30}} to={'/' + PATHS.MENUNOTLOGGEDIN.ABOUT} activeClassName="active">ABOUT</Link>
-               <Link  theme={themeMenu} style={{color:'rgb(136,14,79)', margin:30}} to={'/' + PATHS.MENUNOTLOGGEDIN.CONTACT} activeClassName="active">CONTACT</Link>
-                <Link theme={themeMenu} style={{color:'rgb(136,14,79)', margin:30}} to={'/' + PATHS.MENUNOTLOGGEDIN.REGISTER} activeClassName="active">REGISTER </Link>
-                <Link theme={themeMenu} style={{color:'rgb(136,14,79)', margin:30}}  to={'/' + PATHS.MENUNOTLOGGEDIN.LOGIN} activeClassName="active">Login </Link>
+              <Button icon='bookmark' label='HOME' accent onClick={this.props.home} />
+              <Button icon='bookmark' label='ABOUT' accent onClick={this.props.about} />
+              <Button icon='bookmark' label='CONTACT' accent onClick={this.props.contact} />
+              <Button icon='bookmark' label='LOGIN' accent onClick={this.props.login} />
+              
 
             </Navigation>
           </div>
@@ -40,5 +49,12 @@ class HeaderNotLog extends React.Component {
 );
 };
 }
+HeaderNotLog.propTypes ={
+  home:PropTypes.func,
+  about:PropTypes.func,
+  contact:PropTypes.func,
+  login:PropTypes.func
 
-export default HeaderNotLog
+};
+
+export default connect(null,mapDispatchToProps) (HeaderNotLog)
