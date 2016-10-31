1cdfdfa615e8f1c6aa14b7b43fb0ee1b513f89d5
jpressac <pressaccojuan@gmail.com>
2016-10-31 15:34:33 -0300

    adding new reports

--- public-api/src/main/java/org/iteam/data/dal/meeting/MeetingRepositoryImpl.java
+++ public-api/src/main/java/org/iteam/data/dal/meeting/MeetingRepositoryImpl.java
@@ -37,200 +37,209 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Repository
 public class MeetingRepositoryImpl implements MeetingRepository {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(MeetingRepositoryImpl.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MeetingRepositoryImpl.class);
 
-	private ElasticsearchClient elasticsearchClientImpl;
-	private ExternalConfigurationProperties configuration;
+    private ElasticsearchClient elasticsearchClientImpl;
+    private ExternalConfigurationProperties configuration;
 
-	private static final String IDEA_MEETING_ID_FIELD = "meetingId";
-	private static final String RANKING_ID_FIELD = "ranking";
-	private static final String MEETING_TEAM_NAME_FIELD = "teamName";
-	private static final String PROGRAMMED_DATE_FIELD = "programmedDate";
-	private static final int MAX_RETRIES = 5;
-	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final String IDEA_MEETING_ID_FIELD = "meetingId";
+    private static final String RANKING_ID_FIELD = "ranking";
+    private static final String MEETING_TEAM_NAME_FIELD = "teamName";
+    private static final String PROGRAMMED_DATE_FIELD = "programmedDate";
+    private static final int MAX_RETRIES = 5;
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-	@Override
-	public boolean createMeeting(Meeting meeting) {
-		LOGGER.info("Creating new meeting");
-		LOGGER.debug("Meeting: '{}'", meeting.toString());
+    @Override
+    public boolean createMeeting(Meeting meeting) {
+        LOGGER.info("Creating new meeting");
+        LOGGER.debug("Meeting: '{}'", meeting.toString());
 
-		meeting.setMeetingId(UUID.randomUUID().toString());
-		meeting.setCreationDate(DateTime.now().getMillis());
-		String data = JSONUtils.ObjectToJSON(meeting);
+        meeting.setMeetingId(UUID.randomUUID().toString());
+        meeting.setCreationDate(DateTime.now().getMillis());
+        String data = JSONUtils.ObjectToJSON(meeting);
 
-		IndexResponse response = elasticsearchClientImpl.insertData(data, StringUtilities.INDEX_MEETING,
-				StringUtilities.INDEX_TYPE_MEETING, meeting.getMeetingId());
-		if (!response.isCreated()) {
-			LOGGER.error("The meeting couldn't be created - Meeting: '{}'", meeting.toString());
-			return false;
-		}
-		return true;
+        IndexResponse response = elasticsearchClientImpl.insertData(data, StringUtilities.INDEX_MEETING,
+                StringUtilities.INDEX_TYPE_MEETING, meeting.getMeetingId());
+        if (!response.isCreated()) {
+            LOGGER.error("The meeting couldn't be created - Meeting: '{}'", meeting.toString());
+            return false;
+        }
+        return true;
 
-	}
+    }
 
-	@Override
-	public boolean updateMeeting(Meeting updatedMeeting) {
-		LOGGER.info("Updating meeting");
-		LOGGER.debug("Meeting: '{}'", updatedMeeting.toString());
+    @Override
+    public boolean updateMeeting(Meeting updatedMeeting) {
+        LOGGER.info("Updating meeting");
+        LOGGER.debug("Meeting: '{}'", updatedMeeting.toString());
 
-		String data = JSONUtils.ObjectToJSON(updatedMeeting);
+        String data = JSONUtils.ObjectToJSON(updatedMeeting);
 
-		UpdateResponse response = elasticsearchClientImpl.modifyData(data, StringUtilities.INDEX_MEETING,
-				StringUtilities.INDEX_TYPE_MEETING, updatedMeeting.getMeetingId());
+        UpdateResponse response = elasticsearchClientImpl.modifyData(data, StringUtilities.INDEX_MEETING,
+                StringUtilities.INDEX_TYPE_MEETING, updatedMeeting.getMeetingId());
 
-		// LOGGER.error("The meeting couldn't be updated - Meeting: '{}'",
-		// updatedMeeting.toString());
+        // LOGGER.error("The meeting couldn't be updated - Meeting: '{}'",
+        // updatedMeeting.toString());
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public boolean saveIdeas(IdeasDTO ideas) {
+    @Override
+    public boolean saveIdeas(IdeasDTO ideas) {
 
-		LOGGER.info("Inserting new ideas");
+        LOGGER.info("Inserting new ideas");
 
-		LOGGER.debug("Ideas: '{}'", ideas.toString());
+        LOGGER.debug("Ideas: '{}'", ideas.toString());
 
-		// TODO:check if it's necessary set the insertion date to each idea.
-		List<String> dataToInsert = new ArrayList<>();
+        // TODO:check if it's necessary set the insertion date to each idea.
+        List<String> dataToInsert = new ArrayList<>();
 
-		ideas.getIdeas().forEach((idea) -> {
-			dataToInsert.add(JSONUtils.ObjectToJSON(idea));
-		});
+        ideas.getIdeas().forEach((idea) -> {
+            dataToInsert.add(JSONUtils.ObjectToJSON(idea));
+        });
 
-		// TODO:verify how to check if the document was updated or not. The only
-		// way til now is to execute another query an check the modified fields.
-		elasticsearchClientImpl.insertData(dataToInsert, StringUtilities.INDEX_IDEAS, StringUtilities.INDEX_TYPE_IDEAS);
+        // TODO:verify how to check if the document was updated or not. The only
+        // way til now is to execute another query an check the modified fields.
+        elasticsearchClientImpl.insertData(dataToInsert, StringUtilities.INDEX_IDEAS, StringUtilities.INDEX_TYPE_IDEAS);
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public Reports generateBasicReport(String meetingId) {
-		LOGGER.info("Generating Report");
-		LOGGER.debug("Generating report for meeting: '{}'", meetingId);
+    @Override
+    public Reports generateBasicReport(String meetingId, String fieldOrder, SortOrder sortOrder) {
+        LOGGER.info("Generating Report by " + fieldOrder);
+        LOGGER.debug("Generating report for meeting: '{}'", meetingId);
 
-		BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
-		queryBuilder.must(QueryBuilders.termQuery(IDEA_MEETING_ID_FIELD, meetingId));
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+        queryBuilder.must(QueryBuilders.termQuery(IDEA_MEETING_ID_FIELD, meetingId));
 
-		SearchResponse meetingResponse = elasticsearchClientImpl.search(StringUtilities.INDEX_MEETING,
-				QueryBuilders.termQuery(IDEA_MEETING_ID_FIELD, meetingId));
+        SearchResponse meetingResponse = elasticsearchClientImpl.search(StringUtilities.INDEX_MEETING,
+                QueryBuilders.termQuery(IDEA_MEETING_ID_FIELD, meetingId));
 
-		Reports report = null;
-		List<Idea> ideasList = new ArrayList<>();
-		if (meetingResponse.getHits().getTotalHits() > 0) {
+        Reports report = null;
+        List<Idea> ideasList = new ArrayList<>();
+        if (meetingResponse.getHits().getTotalHits() > 0) {
 
-			try {
-				JsonNode meetingNode = OBJECT_MAPPER.readTree(meetingResponse.getHits().getAt(0).getSourceAsString());
+            try {
+                JsonNode meetingNode = OBJECT_MAPPER.readTree(meetingResponse.getHits().getAt(0).getSourceAsString());
 
-				SearchResponse response = elasticsearchClientImpl.search(StringUtilities.INDEX_IDEAS, queryBuilder,
-						SortBuilders.fieldSort(RANKING_ID_FIELD).order(SortOrder.DESC));
+                SearchResponse response = elasticsearchClientImpl.search(StringUtilities.INDEX_IDEAS, queryBuilder,
+                        SortBuilders.fieldSort(fieldOrder).order(sortOrder));
 
-				if (response.getHits().getTotalHits() > 0) {
-					for (SearchHit hit : response.getHits()) {
-						ideasList.add((Idea) JSONUtils.JSONToObject(hit.getSourceAsString(), Idea.class));
-					}
-				}
+                if (response.getHits().getTotalHits() > 0) {
+                    for (SearchHit hit : response.getHits()) {
+                        ideasList.add((Idea) JSONUtils.JSONToObject(hit.getSourceAsString(), Idea.class));
+                    }
+                }
 
-				report = new Reports(meetingNode.at("/topic").asText(), meetingNode.at("/description").asText(),
-						ideasList);
+                report = new Reports(meetingNode.at("/topic").asText(), meetingNode.at("/description").asText(),
+                        ideasList);
 
-			} catch (JsonProcessingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+            } catch (JsonProcessingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
 
-		return report;
-	}
+        return report;
+    }
 
-	@Override
-	public List<Meeting> getMeetingUser(String username) {
-		LOGGER.info("Getting meeting by user: '{}'", username);
+    @Override
+    public Reports generateBasicReportByUser(String meetingId) {
+        return generateBasicReport(meetingId, "username", SortOrder.ASC);
+    }
 
-		SearchResponse response = elasticsearchClientImpl.search(StringUtilities.INDEX_MEETING,
-				QueryBuilders.termQuery(MEETING_TEAM_NAME_FIELD, username),
-				SortBuilders.fieldSort(PROGRAMMED_DATE_FIELD).order(SortOrder.ASC));
+    @Override
+    public Reports generateBasicReportByTag(String meetingId) {
+        return generateBasicReport(meetingId, "tag", SortOrder.ASC);
+    }
 
-		List<Meeting> meetingList = new ArrayList<>();
+    @Override
+    public List<Meeting> getMeetingUser(String username) {
+        LOGGER.info("Getting meeting by user: '{}'", username);
 
-		LOGGER.debug("meetings retrieved: ", response.getHits().getTotalHits());
+        SearchResponse response = elasticsearchClientImpl.search(StringUtilities.INDEX_MEETING,
+                QueryBuilders.termQuery(MEETING_TEAM_NAME_FIELD, username),
+                SortBuilders.fieldSort(PROGRAMMED_DATE_FIELD).order(SortOrder.ASC));
 
-		for (SearchHit hit : response.getHits()) {
+        List<Meeting> meetingList = new ArrayList<>();
 
-			LOGGER.debug("User '{}' meeting: '{}'", username, hit.getSourceAsString());
+        LOGGER.debug("meetings retrieved: ", response.getHits().getTotalHits());
 
-			meetingList.add((Meeting) JSONUtils.JSONToObject(hit.getSourceAsString(), Meeting.class));
-		}
+        for (SearchHit hit : response.getHits()) {
 
-		LOGGER.debug("User '{}' list of meetings: '{}'", username, meetingList.toString());
+            LOGGER.debug("User '{}' meeting: '{}'", username, hit.getSourceAsString());
 
-		return meetingList;
-	}
+            meetingList.add((Meeting) JSONUtils.JSONToObject(hit.getSourceAsString(), Meeting.class));
+        }
 
-	@Override
-	public List<Meeting> getMeetingByTeamName(List<String> teamName) {
-		List<Meeting> meetingList = new ArrayList<>();
-		LOGGER.info("Getting meeting for teams: '{}'", teamName.toString());
+        LOGGER.debug("User '{}' list of meetings: '{}'", username, meetingList.toString());
 
-		SearchResponse response = elasticsearchClientImpl.search(StringUtilities.INDEX_MEETING,
-				QueryBuilders.termsQuery(MEETING_TEAM_NAME_FIELD, teamName),
-				SortBuilders.fieldSort(PROGRAMMED_DATE_FIELD).order(SortOrder.ASC));
+        return meetingList;
+    }
 
-		for (SearchHit hit : response.getHits()) {
+    @Override
+    public List<Meeting> getMeetingByTeamName(List<String> teamName) {
+        List<Meeting> meetingList = new ArrayList<>();
+        LOGGER.info("Getting meeting for teams: '{}'", teamName.toString());
 
-			Meeting meeting = (Meeting) JSONUtils.JSONToObject(hit.getSourceAsString(), Meeting.class);
-			meetingList.add(meeting);
+        SearchResponse response = elasticsearchClientImpl.search(StringUtilities.INDEX_MEETING,
+                QueryBuilders.termsQuery(MEETING_TEAM_NAME_FIELD, teamName),
+                SortBuilders.fieldSort(PROGRAMMED_DATE_FIELD).order(SortOrder.ASC));
 
-			LOGGER.debug("Meetings: " + meeting.getMeetingId());
-		}
+        for (SearchHit hit : response.getHits()) {
 
-		LOGGER.info("Successfully retrrieve meetings: '{}'", response.getHits().getTotalHits());
+            Meeting meeting = (Meeting) JSONUtils.JSONToObject(hit.getSourceAsString(), Meeting.class);
+            meetingList.add(meeting);
 
-		return meetingList;
-	}
+            LOGGER.debug("Meetings: " + meeting.getMeetingId());
+        }
 
-	@Override
-	public void saveMeetingInfo(String data, String meetingId) {
-		int count = 0;
-		saveInfoRetry(count, data, meetingId);
-	}
+        LOGGER.info("Successfully retrrieve meetings: '{}'", response.getHits().getTotalHits());
 
-	private void saveInfoRetry(int count, String data, String meetingId) {
-		try {
-			elasticsearchClientImpl.insertData(data, StringUtilities.INDEX_MEETING_INFO,
-					StringUtilities.INDEX_TYPE_MEETING_INFO, meetingId);
-			LOGGER.info("Meeting info updated - Meeting: '{}'", meetingId);
-		} catch (ElasticsearchException e) {
-			LOGGER.error("Failed to update meeting info - Retry '{}'", count);
-			if (MAX_RETRIES > count) {
-				saveInfoRetry(count += 1, data, meetingId);
-			}
-		}
-	}
+        return meetingList;
+    }
 
-	@Override
-	public String getMeetingInfo(String meetingId) {
+    @Override
+    public void saveMeetingInfo(String data, String meetingId) {
+        int count = 0;
+        saveInfoRetry(count, data, meetingId);
+    }
 
-		GetResponse response = elasticsearchClientImpl.getDocument(StringUtilities.INDEX_MEETING_INFO,
-				StringUtilities.INDEX_TYPE_MEETING_INFO, meetingId);
-		if (response.isExists()) {
-			return response.getSourceAsString();
-		}
-		return null;
-	}
+    private void saveInfoRetry(int count, String data, String meetingId) {
+        try {
+            elasticsearchClientImpl.insertData(data, StringUtilities.INDEX_MEETING_INFO,
+                    StringUtilities.INDEX_TYPE_MEETING_INFO, meetingId);
+            LOGGER.info("Meeting info updated - Meeting: '{}'", meetingId);
+        } catch (ElasticsearchException e) {
+            LOGGER.error("Failed to update meeting info - Retry '{}'", count);
+            if (MAX_RETRIES > count) {
+                saveInfoRetry(count += 1, data, meetingId);
+            }
+        }
+    }
 
-	@Autowired
-	private void setElasticsearchClientImpl(ElasticsearchClientImpl elasticsearchClientImpl) {
-		this.elasticsearchClientImpl = elasticsearchClientImpl;
-	}
+    @Override
+    public String getMeetingInfo(String meetingId) {
 
-	@Autowired
-	private void setConfiguration(ExternalConfigurationProperties configuration) {
-		this.configuration = configuration;
-	}
+        GetResponse response = elasticsearchClientImpl.getDocument(StringUtilities.INDEX_MEETING_INFO,
+                StringUtilities.INDEX_TYPE_MEETING_INFO, meetingId);
+        if (response.isExists()) {
+            return response.getSourceAsString();
+        }
+        return null;
+    }
 
+    @Autowired
+    private void setElasticsearchClientImpl(ElasticsearchClientImpl elasticsearchClientImpl) {
+        this.elasticsearchClientImpl = elasticsearchClientImpl;
+    }
+
+    @Autowired
+    private void setConfiguration(ExternalConfigurationProperties configuration) {
+        this.configuration = configuration;
+    }
 }
